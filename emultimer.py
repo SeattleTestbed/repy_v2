c69b65e0d6ba67ad65c49d99ff975a8fb411d759
Name <USER@DOMAIN>
2010-01-19 01:36:57 +0000

    Repy V2 sleep and createthread.

--- emultimer.py
+++ emultimer.py
@@ -11,8 +11,6 @@
 
 import threading
 import thread # Armon: this is to catch thread.error
-import time
-import restrictions
 import nanny
 import idhelper
 
@@ -26,12 +24,13 @@ import tracebackrepy
 import harshexit
 
 
-timerinfo = {}
-# Table of timer structures:
-# {'timer':timerobj,'function':function}
+##### Constants
 
 # Armon: Prefix for use with event handles
 EVENT_PREFIX = "_EVENT:"
+
+
+##### Internal Functions
 
 # Generates a valid event handle
 def generate_eventhandle():
@@ -72,7 +71,8 @@ def is_valid_eventhandle(eventhandle):
   return eventhandle.startswith(EVENT_PREFIX)
 
 
-# Public interface!
+##### Public Functions
+
 def sleep(seconds):
   """
    <Purpose>
@@ -92,134 +92,60 @@ def sleep(seconds):
    <Returns>
       None.
   """
-
-  restrictions.assertisallowed('sleep',seconds)
-  
   # Use the do_sleep implementation in misc
   misc.do_sleep(seconds)
 
 
-
-# Public interface!
-def settimer(waittime, function, args):
+def createthread(function):
   """
-   <Purpose>
-      Allow the current event to set an event to be performed in the future.
-      This does not guarantee the event will be triggered at that time, only
-      that it will be triggered after that time.
+  <Purpose>
+    Creates a new thread of execution.
 
-   <Arguments>
-      waittime:
-         The minimum amount of time to wait before delivering the event
-      function:
-         The function to call
-      args:
-         The arguments to pass to the function.   This should be a tuple or 
-         list
+  <Arguments>
+    function:
+      The function to invoke on entering the new thread.
 
-   <Exceptions>
-      None.
+  <Exceptions>
+    RepyArgumentError is raised if the function is not callable.
+    ResourceExhaustedError is raised if there are no available events.
 
-   <Side Effects>
-      None.
+  <Side Effects>
+    Launches a new thread.
 
-   <Returns>
-      A timer handle, for use with canceltimer
+  <Resource Consumption>
+    Consumes an event.
+
+  <Returns>
+    None
   """
-  restrictions.assertisallowed('settimer',waittime)
-  
+  # Check if the function is callable
+  if not callable(function):
+    raise RepyArgumentError, "Provided function is not callable!"
+
+  # Generate a unique handle and see if there are resources available
   eventhandle = generate_eventhandle()
+  nanny.tattle_add_item('events', eventhandle)
 
-  nanny.tattle_add_item('events',eventhandle)
+  # Wrap the provided function
+  def wrapped_func():
+    try:
+      function()
+    except:
+      # Exit if they throw an uncaught exception
+      tracebackrepy.handle_exception()
+      harshexit.harshexit(30)
+    
+    # Remove the event before I exit
+    nanny.tattle_remove_item('events',eventhandle)
 
-  tobj = threading.Timer(waittime,functionwrapper,[function] + [eventhandle] + [args])
+  # Create a thread object
+  tobj = threading.Thread(target=wrapped_func, name=idhelper.get_new_thread_name(EVENT_PREFIX))
 
-  # Set the name of the thread
-  tobj.setName(idhelper.get_new_thread_name(EVENT_PREFIX))
-
-  timerinfo[eventhandle] = {'timer':tobj}
-  
   # Check if we get an exception trying to create a new thread
   try:
-    # start the timer
     tobj.start()
   except thread.error, exp:
     # Set exit code 56, which stands for a Threading Error
     # The Node manager will detect this and handle it
     harshexit.harshexit(56)
-  
-  return eventhandle
-  
-
-# Private function.   This exists to allow me to do quota based items
-def functionwrapper(func, timerhandle, args):
-  #restrictions ?
-  # call the function with the arguments
-  try:
-    if timerhandle in timerinfo:
-      del timerinfo[timerhandle]
-    else:
-      # I've been "stopped" by canceltimer
-      return
-  except KeyError:
-    # I've been "stopped" by canceltimer
-    return
-    
-  try:
-    func(*args)
-  except:
-    # Exit if they throw an uncaught exception
-    tracebackrepy.handle_exception()
-    harshexit.harshexit(30)
-    
-  # remove the event before I exit
-  nanny.tattle_remove_item('events',timerhandle)
-  
-
-
-
-# Public interface!
-def canceltimer(timerhandle):
-  """
-   <Purpose>
-      Cancels a timer.
-
-   <Arguments>
-      timerhandle:
-         The handle of the timer that should be stopped.   Handles are 
-         returned by settimer
-
-   <Exceptions>
-      None.
-
-   <Side Effects>
-      None.
-
-   <Returns>
-      If False is returned, the timer already fired or was cancelled 
-      previously.   If True is returned, the timer was cancelled
-  """
-
-  restrictions.assertisallowed('canceltimer')
-
-  # Armon: Check that the given handle is valid
-  if not is_valid_eventhandle(timerhandle):
-    raise Exception("Invalid timer handle specified!")
-
-  try:
-    timerinfo[timerhandle]['timer'].cancel()
-  except KeyError:
-    # The timer already fired (or was cancelled)
-    return False
-
-  try:
-    del timerinfo[timerhandle]
-  except KeyError:
-    # The timer just fired (or was cancelled)
-    return False
-  else:
-    # I was able to delete the entry, the function will abort.   I can remove
-    # the event
-    nanny.tattle_remove_item('events',timerhandle)
-    return True
-    
+ 
