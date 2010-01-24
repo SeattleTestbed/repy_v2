16b9fe0e81b26c9ac7667497a3d60004f28f94e4
Name <USER@DOMAIN>
2010-01-24 03:28:18 +0000

    Added unit test for randombytes and getruntime (copied)

---
+++ testsV2/z_testgetruntime2.py
@@ -0,0 +1,6 @@
+start = getruntime()
+sleep(.2)
+end = getruntime()
+if end-start < .2:
+  # It didn't sleep long enough or else my counter is wrong
+  print "Elapsed time less than sleep time!"
---
+++ testsV2/z_testgetruntime3.py
@@ -0,0 +1,3 @@
+runtime = getruntime()
+if runtime > 10:
+  print "An immediate call to getruntime() > 10! ("+str(runtime)+")"
