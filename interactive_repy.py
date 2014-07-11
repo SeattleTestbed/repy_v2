9be90c9475fd01e77d2f59a029ed266d6e1ba4d4
Albert <albert.rafetseder+github@univie.ac.at>
2014-07-11 15:10:29 +0200

    Adding a few comments to interactive_repy.py

--- interactive_repy.py
+++ interactive_repy.py
@@ -5,7 +5,7 @@ Useful if you are tired of typing "from repyportability..." and
 "add_dy_support..." into an interactive Python prompt over and over again.
 
 Relevant Python docs:
-https://docs.python.org/2/library/code.html#code.interact
+* https://docs.python.org/2/library/code.html#code.interact
 
 Thank you for the helpful hints:
 * http://stackoverflow.com/questions/13432717/enter-interactive-mode-in-python
@@ -13,23 +13,30 @@ Thank you for the helpful hints:
 
 """
 
+# This gives us the interactive prompt
 import code
-import webbrowser
 
-from repyportability import *
-add_dy_support(locals())
+# This links to the Repy API docs
+import webbrowser
 
 def repy_api_help():
   webbrowser.open("https://seattle.poly.edu/wiki/RepyV2API")
 
+# These make the Repy API and dy_import_module* functions available 
+# on the interactive prompt
+from repyportability import *
+add_dy_support(locals())
+
+# Launch the console!
 code.interact(local=locals(), banner="""
 Welcome to the Interactive RepyV2 console!
-(Type ``repy_api_help()'' to open the Repy API documentation at 
+
+Type ``repy_api_help()'' to open the Repy API documentation at 
 https://seattle.poly.edu/wiki/RepyV2API in a browser window, and 
-visit https://github.com/SeattleTestbed for the latest code updates.)
+visit https://github.com/SeattleTestbed for the latest code updates.
 """)
 
 # Once they are done, ...
-print """
-Thanks for using the Interactive RepyV2 console. Have a great day!"""
+print """Thanks for using the Interactive RepyV2 console. Have a great day!
+"""
 
