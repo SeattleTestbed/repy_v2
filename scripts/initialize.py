9c42810505fe6770b6a1e773482aefe739c748a0
Chintan Choksi <chintan@Chintans-MacBook-Pro.local>
2014-08-04 11:01:35 -0400

    Scripts for building Seattle Components

--- scripts/initialize.py
+++ scripts/initialize.py
@@ -29,7 +29,8 @@ import os
 import sys
 
 
-alist = [line.rstrip('\n') for line in open('config_initialize.txt')]
+lines = [line.rstrip('\n') for line in open('config_initialize.txt')]
+alist = list(line for line in lines if line)
 length = len(alist)
 
 count =0
@@ -38,27 +39,26 @@ while(count < length):
   alist0=repo.split(' ', 1)[0]
   #print alist0
   if alist0 != "test" and alist0 != "install" and alist0 != "#":
-    pr = subprocess.Popen("git clone https://github.com/SeattleTestbed/"+ alist[count] , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+    pr = subprocess.Popen("git clone "+ alist[count] , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
     (out, error) = pr.communicate()
+    print str("Checking out repo:")
+    print alist[count]
     count =count+1
-    print str("Checking out repo")
-    print count
   elif alist0 == "install":
     repo=alist[count]
     alist1=repo.split(' ', 1)[1]
+    pr = subprocess.Popen("pip install "+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+    pr = subprocess.Popen("git clone "+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+    print str("Checking out repo:")
+    print alist1
     count=count+1
-    pr = subprocess.Popen("pip install git+https://github.com/SeattleTestbed/"+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
-    pr = subprocess.Popen("git clone https://github.com/SeattleTestbed/"+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
-    print str("Checking out repo")
-    print count
   elif alist0 == "test":
     repo=alist[count]
     alist1=repo.split(' ', 1)[1]
-    count=count+1
-    pr = subprocess.Popen("pip install git+git://github.com/SeattleTestbed/"+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
-    pr = subprocess.Popen("git clone https://github.com/SeattleTestbed/"+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+    pr = subprocess.Popen("git clone "+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
     print str("Checking out repo")
-    print count
+    print alist1
+    count=count+1
   
 
 
