a1c6404254fa126448994cefe35cc15193267b4c
Chintan Choksi <chintan.choksi22@gmail.com>
2014-08-13 22:05:56 +0400

    Replaced initialize.py with a newer version

--- scripts/initialize.py
+++ scripts/initialize.py
@@ -1,7 +1,4 @@
-
 """
-# !/usr/bin/python
-
 <Program>
   initialize.py 
   
@@ -12,56 +9,33 @@
   Chintan Choksi
 
 <Purpose>
-  This script does a git check-out of all the dependent repositories to the home directory.
+  This script does a git check-out of all the dependent repositories 
+  to the current directory.
   
 <Usage>
-  How to run this program:
-  1. Clone the dist repository to local machine, and run this script
-  2. To Run this script: 
-  dist user$ python initialize.py
+  * Clone the repository you would like to build on your machine, e.g. using 
+      ``git clone https://github.com/SeattleTestbed/seash''
+  * Change into the ``scripts'' subdirectory
+  * Run this script: 
+      ``python initialize.py''
+  * The dependencies will be checked out into the current directory.
    
 """
 
-
-import commands
 import subprocess
 import os
-import sys
 
+config_file = open("config_initialize.txt")
 
-lines = [line.rstrip('\n') for line in open('config_initialize.txt')]
-alist = list(line for line in lines if line)
-length = len(alist)
+for line in config_file.readlines():
+  # Ignore comments and blank lines
+  if line.startswith("#") or line=='\n':
+    continue
 
-count =0
-while(count < length):
-  repo=alist[count]
-  alist0=repo.split(' ', 1)[0]
-  #print alist0
-  if alist0 != "test" and alist0 != "install" and alist0 != "#":
-    pr = subprocess.Popen("git clone "+ alist[count] , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
-    (out, error) = pr.communicate()
-    print str("Checking out repo:")
-    print alist[count]
-    count =count+1
-  elif alist0 == "install":
-    repo=alist[count]
-    alist1=repo.split(' ', 1)[1]
-    pr = subprocess.Popen("pip install "+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
-    pr = subprocess.Popen("git clone "+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
-    print str("Checking out repo:")
-    print alist1
-    count=count+1
-  elif alist0 == "test":
-    repo=alist[count]
-    alist1=repo.split(' ', 1)[1]
-    pr = subprocess.Popen("git clone "+ alist1 , cwd = os.getcwd(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
-    print str("Checking out repo")
-    print alist1
-    count=count+1
-  
-
-
-
-
+  # If we end up here, the line contains a Git URL (+options?) for us to clone
+  print "Checking out repo from", line.split()[0], "..."
+  pr = subprocess.Popen("git clone " + line, cwd = os.getcwd(), shell = True, 
+     stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+  (out, error) = pr.communicate()
+  print "Done!"
 
