4d3ff164ab5596767bce3174e089f2a4778106e8
kellender <ajyshenoy@gmail.com>
2015-07-23 11:50:29 -0400

    250 file handle exhaust test

--- testsV2/ut_repyv2api_openfileconsumesfilehandles.py
+++ testsV2/ut_repyv2api_openfileconsumesfilehandles.py
@@ -3,15 +3,14 @@ This test checks the behavior of openfile when filehandles should be exhausted.
 """
 
 #pragma repy
+filehandles = []
 
-MAX_FILES = 5
 
-files = listfiles()
-filehandles = [None, None, None, None, None]
+RAND_FILE = "random.files.to.create.number"
 
 # Open random files
-for x in xrange(MAX_FILES):
-  filehandles[x] = openfile(files[x], False)
+for x in xrange(getresources()[0]['filesopened']):
+  filehandles.append(openfile((RAND_FILE + str(x + 1)), True)) # the plus 1 is for getting to 250 files!
 
 # Try to open this file with create, we should get an error
 # and the file should not be created
@@ -27,4 +26,3 @@ else:
 # Check if the file exists
 if TRY_FILE in listfiles():
   log("The file was created with no handles available!",'\n')
-
