05c346ed42e8f326a0517b0053ba67530c9ca167
Ajay Shenoy <ajyshenoy@gmail.com>
2015-11-24 11:21:24 -0500

    Fixed code style issue

--- testsV2/ut_repyv2api_openfileconsumesfilehandles.py
+++ testsV2/ut_repyv2api_openfileconsumesfilehandles.py
@@ -6,28 +6,28 @@ This test checks the behavior of openfile when filehandles should be exhausted.
 filehandles = []
 filenames = []
 
-RAND_FILE = "random.files.to.create.number"
+rand_files_name = "random.files.to.create.number"
 
 # Open random files, as many as the resource restrictions allows  
 for number in xrange(getresources()[0]['filesopened']):
-  filenames.append(RAND_FILE + str(number + 1))
-  filehandles.append(openfile((RAND_FILE + str(number + 1)), True))
+  filenames.append(rand_files_name + str(number + 1))
+  filehandles.append(openfile((rand_files_name + str(number + 1)), True))
 
 # Try to open this file with create, we should get an error
 # and the file should not be created
-TRY_FILE = "trying.to.create.this.file"
+tryfile = "trying.to.create.this.file"
 
 try:
-  fileh = openfile(TRY_FILE, True)
+  fileh = openfile(tryfile, True)
   fileh.close()
 except ResourceExhaustedError:
   pass
 else:
   log("Opened file with no handles available!",'\n')
 # Check if the file exists
-if TRY_FILE in listfiles():
+if tryfile in listfiles():
   log("The file was created with no handles available!",'\n')
-  removefile(TRY_FILE)
+  removefile(tryfile)
 
 #Close all random files opened
 for randFile in filehandles:
