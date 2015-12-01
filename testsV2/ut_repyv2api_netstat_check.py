b0a17896cfb49c3f15cb0668d8de48ed1634c83f
kellender <ajyshenoy@gmail.com>
2015-12-01 10:41:25 -0500

    Comments

--- testsV2/ut_repyv2api_netstat_check.py
+++ testsV2/ut_repyv2api_netstat_check.py
@@ -1,6 +1,13 @@
+# This unit test tests to see if netstat is installed on the machine
+
+# If this unit test fails then other modules that performs checks
+# over the network will fail
+# network connections both incoming and outgoing 
+# as well as viewing routing tables, interface statistics 
+# become unverifiable
+ 
+
 import portable_popen
-
-
 
 try:
   # check if netstat exists
